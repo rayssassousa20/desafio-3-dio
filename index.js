c30b99cab54c9c55fe6058d87bc49cc91cad2ee6
rayssassousa20 <rayssbtx1@gmail.com>
2024-01-17 15:48:34 -0300

    last version of the challenge

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("hi")
+class hero{
+    constructor(nome, idade, tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    ataque(){
+        let attack
+        if(this.tipo == "mago"){
+            attack = "magia"
+        } else if (this.tipo == "guerreiro"){
+            attack = "espada"
+        } else if (this.tipo == "monge"){
+            attack = "artes marciais"
+        } else if (this.tipo == "ninja"){
+            attack = "shuriken"
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${attack}`)
+    }
+}
+
+
+let displayHeroAttack = new hero("João", 22, "monge")
+
+displayHeroAttack.ataque()
